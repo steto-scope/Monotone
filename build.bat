e8f2a17618c8259312b2f8e4d3fb6850248ec117
recursivebytes <error0x2b1@googlemail.com>
2014-08-14 23:07:52 +0200

    Several minor fixes

--- build.bat
+++ build.bat
@@ -2,6 +2,6 @@ copy "D:\vs\Monotone\Monotone\Monotone.Colors.xaml" "D:\vs\Monotone\Monotone\bin
 copy "D:\vs\Monotone\Monotone\Monotone.Brushes.xaml" "D:\vs\Monotone\Monotone\bin\Release\Monotone.Brushes.xaml"
 del "D:\vs\Monotone\Monotone.zip"
 del "D:\vs\Monotone\Release.zip"
-D:\vs\Monotone\7za.exe a -tzip D:\vs\Monotone\Monotone.zip D:\vs\Monotone\Monotone\Monotone.Colors.xaml D:\vs\Monotone\Monotone\Monotone.Brushes.xaml D:\vs\Monotone\Monotone\Monotone.MahApps.xaml D:\vs\Monotone\Monotone\Monotone.xaml D:\vs\Monotone\Monotone\Monotone.ExtendedWPFToolkit.xaml D:\vs\Monotone\Monotone\MonotoneUtils.cs D:\vs\Monotone\MonotoneTheme\bin\Release\MonotoneTheme.dll
+D:\vs\Monotone\7za.exe a -tzip D:\vs\Monotone\Monotone.zip D:\vs\Monotone\Monotone\Monotone.Colors.xaml D:\vs\Monotone\Monotone\Monotone.Brushes.xaml D:\vs\Monotone\Monotone\Monotone.MahApps.xaml D:\vs\Monotone\Monotone\Monotone.xaml D:\vs\Monotone\Monotone\Monotone.ExtendedWPFToolkit.xaml D:\vs\Monotone\Monotone\Monotone.ColorBox.xaml D:\vs\Monotone\Monotone\MonotoneUtils.cs D:\vs\Monotone\MonotoneTheme\bin\Release\MonotoneTheme.dll
 D:\vs\Monotone\7za.exe a -tzip D:\vs\Monotone\Release.zip D:\vs\Monotone\Monotone\bin\Release\*
 exit
